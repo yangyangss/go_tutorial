968261c5246b67dda67e32f3f6216d97fe9ac3f8
yangyangss <chevy.yang@gmail.com>
2021-08-15 22:28:01 +0800

    add maps example

---
+++ 10_maps/maps.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := make(map[string]int)
+
+	m["k1"] = 7
+	m["k2"] = 13
+
+	fmt.Println("map: ", m)
+
+	v1 := m["k1"]
+	fmt.Println("v1: ", v1)
+
+	fmt.Println("len", len(m))
+
+	_, prs := m["k2"]
+	fmt.Println("prs:", prs)
+
+}
