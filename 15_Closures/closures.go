9bf0a93f89d3dc49afa533f9e8990bd520559ac2
yangyangss <chevy.yang@gmail.com>
2021-08-17 00:18:30 +0800

    add closures example

---
+++ 15_Closures/closures.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+///Go supports anonymous functions, which can form closures.
+///Anonymous functions are useful when you want to define a function inline without having to name it.
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+	nextInt := intSeq()
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInts := intSeq()
+	fmt.Println(newInts())
+}
